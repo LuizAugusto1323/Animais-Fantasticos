781f4f34dc431ce1a89e1d4b7003678d2a176747
Luiz Augusto <luiz.kirk4@gmail.com>
2023-01-30 18:03:49 -0300

    tabNav tranformado em classe

--- js/modules/tabNav.js
+++ js/modules/tabNav.js
@@ -1,31 +1,29 @@
-// colocamos tudo dentro de uma função para isolar o
-// codigo e não haver conflito entre nomes de variaveis e etc
-export default function initTabNav() {
-  // seleciona a lista de imagens
-  const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
-
-  // seleciona as sessões (texto ao lado)
-  const tabContent = document.querySelectorAll('[data-tab="content"] section');
-  function activeTab(index) {
-    tabContent.forEach((section) => {
-      section.classList.remove('ativo');
-    });
-    const direcao = tabContent[index].dataset.anime;
-    tabContent[index].classList.add('ativo', direcao);
+export default class initTabNav {
+  constructor(menu, content) {
+    this.tabMenu = document.querySelectorAll(menu);
+    this.tabContent = document.querySelectorAll(content);
+    this.activeClass = 'ativo';
   }
-  // faz uma verificação e executa a função caso retorne true
-  if (tabMenu.length && tabContent.length) {
-    // adiciona a classe ativo ao primeiro item para nao ficar tudo em branco
-    tabContent[0].classList.add('ativo');
 
-    // para cada sessao, remova a classe ativo caso houver e depois adicione
-    // obs, funciona dessa maneira pq tanto as sessões e as imagens tem o
-    // mesmo numero de index/itens para cada imagem, adicione o evento
-    // click e quando ocorrer chame a funçãp a cima
-    tabMenu.forEach((itemMenu, index) => {
-      itemMenu.addEventListener('click', () => {
-        activeTab(index);
-      });
+  activeTab(index) {
+    this.tabContent.forEach((section) => {
+      section.classList.remove(this.activeClass);
     });
+    const direcao = this.tabContent[index].dataset.anime;
+    this.tabContent[index].classList.add(this.activeClass, direcao);
+  }
+
+  // adiciona os eventos as tabs
+  addTabNavEvent() {
+    this.tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => this.activeTab(index));
+    });
+  }
+
+  init() {
+    if (this.tabMenu.length && this.tabContent.length) {
+      this.activeTab(0);
+      this.addTabNavEvent();
+    }
   }
 }
