e10f26e391f0db1978231b83b2eb4fa216dad2a3
Luiz Augusto <luiz.kirk4@gmail.com>
2023-01-30 17:37:26 -0300

    Accordion tranformado em classe

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 import ScrollSuave from './modules/scrollSuave.js';
-import initAnimacaoScroll from './modules/scrollAnimacao.js';
-import initAccordion from './modules/accordion.js';
+import Accordion from './modules/accordion.js';
 import initTabNav from './modules/tabNav.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
@@ -8,13 +7,15 @@ import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import initFetchAnimais from './modules/fetchAnimais.js';
+import initAnimacaoScroll from './modules/scrollAnimacao.js';
 import initFetchBitcoin from './modules/fetchBitcoin.js';
 
 const scrollsuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollsuave.init();
 
-initAnimacaoScroll();
-initAccordion();
+const accordion = new Accordion('[data-anime="accordion"] dt');
+accordion.init();
+
 initTabNav();
 initModal();
 initTooltip();
@@ -23,3 +24,4 @@ initMenuMobile();
 initFuncionamento();
 initFetchAnimais();
 initFetchBitcoin();
+initAnimacaoScroll();
